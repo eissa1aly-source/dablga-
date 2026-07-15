8fdff9b2ae0da48ae3c811aff7a260481b6038f8
eissa1aly-source <eissa1aly@gmail.com>
2026-07-16 01:56:52 +0300

    Add share option to demo.launch parameters

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from app import demo
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
-    demo.launch(server_name="0.0.0.0", server_port=port)
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=port,
+        share=False
+    )

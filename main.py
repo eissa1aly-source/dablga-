2f29053ed11da4f0d0c0b7ad86f514c9ba38a3a7
eissa1aly-source <eissa1aly@gmail.com>
2026-07-16 01:17:48 +0300

    Use environment variable for server port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# main.py
+import os
 from app import demo
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
